a461a9f179e068fe7ad363b4c656b75380936719
ikhsanmaaa <ikhsanmaaa@gmail.com>
2025-04-09 22:59:33 +0700

    initial commit

--- MenuItem/tests.py
+++ MenuItem/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .models import MenuItem
 # Create your tests here.
+
+class MenuTestCase(TestCase):
+    def test_menu_creation(self):
+        menu = MenuItem.objects.create(
+            title='macaron', 
+            price = 8.4,
+            featured = False,
+            category = None
+            )
+        self.assertEqual(menu.title,'macaron')
